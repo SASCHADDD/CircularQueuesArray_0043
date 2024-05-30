4f556023d6d85a638142a2dbbcd93682fce28918
SASCHADDD <saschadanu23@gmail.com>
2024-05-30 18:25:01 +0700

    VOID DISPLAY

--- circulararray.cpp
+++ circulararray.cpp
@@ -1,43 +1,75 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Queues {
+class Queues
+{
     int FRONT, REAR, max = 5;
     int queue_array[5];
 
 public:
-    Queues(){
+    Queues()
+    {
         FRONT = -1;
         REAR = -1;
     }
-    void insert() {
+
+    void insert()
+    {
         int num;
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
 
-        // cek antrian penuh
-        if (REAR == max - 1 )
-            REAR = 0;
-        else
-            REAR = REAR + 1
-    }
-    int queue_array [ REAR ] = num;
-        
- }
+        //cek apakah antrian penuh?
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
 
-    void remove(){
-        //cek apakah antrian kosong
-        if (FRONT == -1){
+        //cek apakah antrian kosong?
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num; //menambahkan elemen ke antrian
+    }
+
+    void remove()
+    {
+        //cek apakah antrian kosong?
+        if (FRONT == -1)
+        {
             cout << "Queue underflow\n";
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
 
-        // cek jika antrian hanya memiliki satu element
-        if (FRONT == REAR){
-            FRONT = 0;
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
         else
-            FRONT = FRONT + 1;
+        {
+            //jika elemen yg dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
         }
     }
+        void display(){
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+        }
