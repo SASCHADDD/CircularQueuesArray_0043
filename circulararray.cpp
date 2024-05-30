3e75bd06e2f884be9d2f8096f4288aef2a4e691b
SASCHADDD <saschadanu23@gmail.com>
2024-05-30 18:42:20 +0700

    int main dengan memanggil queues insial q dan char inisal ch

--- circulararray.cpp
+++ circulararray.cpp
@@ -110,4 +110,17 @@ public:
 int main(){
     Queues q;
     char ch;
+
+    while (true){
+        try{
+            cout << "Menu" << endl;
+            cout << "1. Implement Insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cin >> ch;
+            cout << endl;
+
+        }
+    }
 }
