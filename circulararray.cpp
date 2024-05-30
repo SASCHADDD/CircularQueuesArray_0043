e04da9c2021c5f3fee345c02aa89f180be73a325
SASCHADDD <saschadanu23@gmail.com>
2024-05-30 18:37:23 +0700

    Iterasi dari awal array hingga REAR

--- circulararray.cpp
+++ circulararray.cpp
@@ -96,5 +96,17 @@ public:
                 cout << queue_array[FRONT_position] << " ";
                 FRONT_position++;
             }
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position){
+                cout << queue_array[FRONT_position] <<" ";
+            }
+            cout << endl;
         }
-        }
+    }
+};
+
+int main(){
+    
+}
